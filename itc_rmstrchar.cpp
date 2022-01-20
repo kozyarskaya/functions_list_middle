50d2062755ff336f800881a45c7a1d89567035d2
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2022-01-20 12:24:35 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -2,17 +2,20 @@
 
 string itc_rmstrchar(string str, string less1){
     string new_str = str;
-    int ind = itc_find_str(new_str, less1), start = 0;
+    int start = 0, ind = 0;
     int lon = itc_len(less1);
     if (less1 == "")
         return str;
     if (less1 == str)
         return "";
+    for (int i = 0; i < lon; i++){
+        ind = itc_find_str(str, less1[i]);
     while(ind != -1){
         new_str = itc_slice_str(str, start, ind - 1);
-        new_str += itc_slice_str(str, ind + lon, itc_len(str));
-        ind = itc_find_str(new_str, less1);
-        str = new_str;
+        new_str += itc_slice_str(str, ind + 1, itc_len(str));
+        ind = itc_find_str(new_str, less1[i]);
+        str = new_str; 
+    }
     }
     return new_str;
 }
