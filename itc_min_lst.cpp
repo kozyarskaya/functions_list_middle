387f0201c9d08efaffe517464b3cff8397c98b9a
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2022-01-20 09:21:59 +0300

    Update itc_min_lst.cpp

--- itc_min_lst.cpp
+++ itc_min_lst.cpp
@@ -1,12 +1,12 @@
 #include "middle_list.h"
 
 int itc_min_lst(const vector <int> &lst){
-    int mann = 2147483647;
+    int minn = 2147483647;
     if (lst.size() > 0){
         for (int i = 0; i < lst.size(); i++){
-            if (lst[i] < mann)
-                mann = lst[i];
+            if (lst[i] < minn)
+                minn = lst[i];
         }
     }
-    return mann;
+    return minn;
 }
