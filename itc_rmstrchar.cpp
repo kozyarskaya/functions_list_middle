e98c84d568691337d73348b4175353eec21cf93c
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2022-01-20 11:14:08 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -4,6 +4,8 @@ string itc_rmstrchar(string str, string less1){
     string new_str = str;
     int ind = itc_find_str(new_str, less1), start = 0;
     int lon = itc_len(less1);
+    if (less1 == "")
+        return str;
     while(ind != -1){
         new_str = itc_slice_str(str, start, ind - 1);
         new_str += itc_slice_str(str, ind + lon, itc_len(str));
@@ -12,3 +14,4 @@ string itc_rmstrchar(string str, string less1){
     }
     return new_str;
 }
+
