d71117ed3e4e18b61589a790040d3a5553f92a43
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2022-01-20 12:25:24 +0300

    Update itc_find_str.cpp

--- itc_find_str.cpp
+++ itc_find_str.cpp
@@ -1,21 +1,13 @@
 #include "middle_list.h"
 
-int itc_find_str(string str1, string str2){
+int itc_find_str(string str1, char str2){
     long long len1 = 0;
     len1 =  itc_len(str1);
     long long len2 = 0;
-    len2 = itc_len(str2);
     long long kol = 0;
     for (long long i = 0; i < len1; i++){
         kol = 0;
-        if (str1[i] == str2[0]){
-        for (long long j = 0; j < len2; j++){
-            if (str1[i + j] == str2[j]){
-                kol += 1;
-            }
-        }
-        }
-        if (kol == len2){
+        if (str1[i] == str2){
             return i;
         }
     }
